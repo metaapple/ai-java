e8019c60bfad70bc81c90bc629c9f9b6c948fb6e
hera <hera@naver.com>
2026-05-05 12:09:01 +0900

    코드 수정3

--- src/test/SimpleCalculator.java
+++ src/test/SimpleCalculator.java
@@ -9,7 +9,7 @@ import java.net.URL;
 public class SimpleCalculator extends JFrame {
     private JTextField num1Field;
     private JTextField num2Field;
-    private JLabel2 label;
+    private JLabel22 label;
 
 
     public SimpleCalculator() {
